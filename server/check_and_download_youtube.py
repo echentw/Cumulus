ed39f8032c5fd38a2b297b223cc3099af985a1a3
Eric Chen <ericchen098@gmail.com>
2018-01-16 21:36:38 -0800

    Revert "use tmp directory for downloads in progress on server"
    This reverts commit dbce8a1c48f92c349aacfa68e58593a68b127579.

--- server/check_and_download_youtube.py
+++ server/check_and_download_youtube.py
@@ -4,15 +4,8 @@ import subprocess
 import argparse
 import os.path
 
-PENDING_DIR = 'pending_downloads'
-DOWNLOADS_DIR = 'downloads'
-
-def song_is_downloaded(videoId):
-  path = os.path.join(DOWNLOADS_DIR, 'song_{}.mp3'.format(videoId))
-  return os.path.isfile(path)
-
 def check_and_download(videoId):
-  if song_is_downloaded(videoId):
+  if os.path.isfile('downloads/song_' + videoId + '.mp3'):
     print('video already exists')
     return
 
@@ -21,14 +14,9 @@ def check_and_download(videoId):
     '-f', 'webm',
     '--extract-audio',
     '--audio-format', 'mp3',
-    '--output', os.path.join(PENDING_DIR, 'song_{}.%(ext)s'.format(videoId)),
-    'https://www.youtube.com/watch?v={}'.format(videoId),
+    '--output', 'downloads/song_' + videoId + '.%(ext)s',
+    'https://www.youtube.com/watch?v=' + videoId,
   ])
-
-  pending_path = os.path.join(PENDING_DIR, 'song_{}.mp3'.format(videoId))
-  download_path = os.path.join(DOWNLOADS_DIR, 'song_{}.mp3'.format(videoId))
-  os.rename(pending_path, download_path)
-
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
